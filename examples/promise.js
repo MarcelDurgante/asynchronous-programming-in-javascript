fe8aa8fdc09ddf37bb166258b321ed06479656bb
MarcelDurgante <marceldur@gmail.com>
2023-11-03 10:26:40 +0100

    Comments about Promises

---
+++ examples/promise.js
@@ -0,0 +1,8 @@
+// Promise: an object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+// Readable asynchronous code.
+
+// Promises States: Pending, Fulfilled and Rejected.
+
+// Promise settled or resolved means a promise is either fulfilled or rejected, but no longer pending.
+
