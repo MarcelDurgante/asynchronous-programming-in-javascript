5f36c237e714f817cac3938d4e90224235bf274b
MarcelDurgante <marceldur@gmail.com>
2023-11-06 23:20:52 +0100

    Handling Errors with Promises

--- public/orders.js
+++ public/orders.js
@@ -1,3 +1,8 @@
-axios.get('http://localhost:3000/api/orders').then(({data}) => {
-  showOrderList('#order-list', data);
-})
+// requesting an order that does not exist (/999) to se how to "Handling Errors with Promises"
+axios.get("http://localhost:3000/api/orders/999").then(({ data }) => {
+  if (XPathResult.status === 200) {
+    showOrderList("#order-list", data);
+  } else {
+    showError("Error!");
+  }
+});
