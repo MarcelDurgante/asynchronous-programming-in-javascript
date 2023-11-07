a5abca1069465384db26f106a9d02d675732a8c3
MarcelDurgante <marceldur@gmail.com>
2023-11-07 01:40:11 +0100

    Formated code with prettier

--- public/orders.js
+++ public/orders.js
@@ -5,17 +5,18 @@ axios
   .get("http://localhost:3000/api/orderStatuses")
   .then(({ data }) => {
     statuses = data;
-    return axios.get("http://localhost:3000/api/orders"); 
+    return axios.get("http://localhost:3000/api/orders");
   })
   .then(({ data }) => {
     let orders = data.map((o) => {
       return {
         ...o,
-        orderStatus: statuses.find((d) => d.id === o.orderStatusId).description, 
+        orderStatus: statuses.find((d) => d.id === o.orderStatusId).description,
       };
     });
     showOrderList("#order-list", orders);
-  }).catch ((err) => showError('#order-list', err))
+  })
+  .catch((err) => showError("#order-list", err))
   .finally(() => {
     setTimeout(hideWaiting, 1500);
-  })
+  });
