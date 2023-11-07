aff76c9da5dd3158028e5e286810902e082ba0c2
MarcelDurgante <marceldur@gmail.com>
2023-11-07 01:34:47 +0100

    Error output, explanation and example of a valida promise chain

--- public/orders.js
+++ public/orders.js
@@ -22,3 +22,24 @@ axios
 
   
   */
+
+/* Valid promise Chain: 
+  
+  promise
+    .the()
+    .catch()
+    .then()
+    .then()
+    .catch()
+
+In this situation, the first catch will only handle errors that happen in the first then function. 
+
+However, //* the second catch will handle errors that happen in any of the then functions, including the first one. And it will also handle errors that might happen in your first catch function. 
+
+As you start to chain promises together, you might want some code to run only once the entire chain is settled. 
+
+You could have a lot of duplicated code in each then and catch block to ensure that you're capturing that the promise is completely settled. 
+
+Or, as I'll show you next, you could accomplish that with a //> built‑in promise function.
+  
+*/
