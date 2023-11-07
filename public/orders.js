1e2e7d3082407dcac4d5e30bae583e90bdd74346
MarcelDurgante <marceldur@gmail.com>
2023-11-07 06:19:44 +0100

    Handling Errros with Async / Await

--- public/orders.js
+++ public/orders.js
@@ -1,3 +1,9 @@
+/* Handling Errros with Async / Await: 
+
+When handling errors with promises, you only had to attach a catch function. But how do you handle errors with async/await? 
+
+*/
+
 showWaiting();
 async function get() {
   const { data } = await axios.get("http://localhost:3000/api/orders");
