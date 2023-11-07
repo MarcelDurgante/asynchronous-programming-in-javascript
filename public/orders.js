47155c86db4256b6edda3bde86e62dca8e06317d
MarcelDurgante <marceldur@gmail.com>
2023-11-07 01:35:22 +0100

    Fix the code and delete comments

--- public/orders.js
+++ public/orders.js
@@ -4,42 +4,14 @@ axios
   .get("http://localhost:3000/api/orderStatuses")
   .then(({ data }) => {
     statuses = data;
-    return axios.get("http://localhost:3000/api/orders"); // put back the return statment
+    return axios.get("http://localhost:3000/api/orders"); 
   })
   .then(({ data }) => {
     let orders = data.map((o) => {
       return {
         ...o,
-        orderStatus: statuses.find((d) => d.id === o.order.orderStatusId).description, // add 'order' to cause an error 
+        orderStatus: statuses.find((d) => d.id === o.orderStatusId).description, 
       };
     });
     showOrderList("#order-list", orders);
   }).catch ((err) => showError('#order-list', err)); 
-
-  /* output: 
-  
-  TypeError: Cannot read properties of undefined (reading 'orderStatusId')
-
-  
-  */
-
-/* Valid promise Chain: 
-  
-  promise
-    .the()
-    .catch()
-    .then()
-    .then()
-    .catch()
-
-In this situation, the first catch will only handle errors that happen in the first then function. 
-
-However, //* the second catch will handle errors that happen in any of the then functions, including the first one. And it will also handle errors that might happen in your first catch function. 
-
-As you start to chain promises together, you might want some code to run only once the entire chain is settled. 
-
-You could have a lot of duplicated code in each then and catch block to ensure that you're capturing that the promise is completely settled. 
-
-Or, as I'll show you next, you could accomplish that with a //> built‑in promise function.
-  
-*/
