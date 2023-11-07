b221d87e77361d095b7991db72f58efe7cb7126c
MarcelDurgante <marceldur@gmail.com>
2023-11-07 06:41:30 +0100

    Output comment

--- public/orders.js
+++ public/orders.js
@@ -2,20 +2,37 @@
 
 Promises can be chained to combat race conditions. Let's see how to accomplish the same thing with async/await.
 
-
-
 */
 
 showWaiting();
 
 async function get() {
   try {
+    //  Inside of our try block, let's add a call to get our order statuses
+    const { data: statuses } = await axios.get(
+      "http://localhost:3000/api/orderStatuses"
+    );
     const { data } = await axios.get("http://localhost:3000/api/orders/");
-    showOrderList("#order-list", data);
+    // At this point, we have both of our statuses and our orders, so we can loop over them and assign the correct status to each order.
+    const orders = data.map((o) => {
+      return {
+        ...o,
+        orderStatus: statuses.find((d) => d.id === o.orderStatusId).description,
+      };
+    });
+
+    showOrderList("#order-list", orders); // And then we just need to make sure that we're passing the orders value to the showOrderList function
   } catch (err) {
     showError("#order-list", err);
   }
-};
+}
 
 get();
 hideWaiting();
+
+/* output: 
+
+We again get our list of orders, each with an order status, which is what we'd expect. 
+And when we look at our Network tab in the console, we see that the order status call is first, and the orders call is second, again what we would expect since we sequenced our API calls in this order.
+
+*/
