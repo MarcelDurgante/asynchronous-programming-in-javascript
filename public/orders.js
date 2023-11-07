a60702c84251b33c8f3081c4d4f19d2821077af6
MarcelDurgante <marceldur@gmail.com>
2023-11-07 01:23:43 +0100

    Catching Errors in a Chain
    Forced error deleting 'return' keyword from the second axios get and added .cath to handle the error and print to the screen so it doesn't buble up to the users console.

--- public/orders.js
+++ public/orders.js
@@ -4,7 +4,7 @@ axios
   .get("http://localhost:3000/api/orderStatuses")
   .then(({ data }) => {
     statuses = data;
-    return axios.get("http://localhost:3000/api/orders");
+    axios.get("http://localhost:3000/api/orders"); // In order to force an error, let's start by deleting the return keyword when we're fetching our orders. This should cause an error with our second then function because data won't be defined. 
   })
   .then(({ data }) => {
     let orders = data.map((o) => {
@@ -14,5 +14,12 @@ axios
       };
     });
     showOrderList("#order-list", orders);
-    console.table(orders); 
-  });
+  }).catch ((err) => showError('#order-list', err)); // Next, we want to add a catch function. The question is where do we want to add it? Well let's start by placing it after the second then function
+
+  // upon realoding the page the output is printed to the screen and will not see the order table and nothing in the console as we handled the error using '.catch'
+
+  /* output: 
+  
+  TypeError: Cannot destructure property 'data' of 'undefined' as it is undefined.
+  
+  */
