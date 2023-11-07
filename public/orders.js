0d0d622e1cfdc35e29763828c0d1afe8e92de497
MarcelDurgante <marceldur@gmail.com>
2023-11-07 01:04:24 +0100

    Deleted comment

--- public/orders.js
+++ public/orders.js
@@ -16,19 +16,3 @@ axios
     showOrderList("#order-list", orders);
     console.table(orders); 
   });
-
-  /* output: 
-  
-(index)     id     orderStatusId     userId     shippingAddress    billingAddress    itemIds     total         date                 orderStatus
-0	          1      1	               2	        3	                 4	               Array(2)    '$318.00'	   'Dec 12, 2020'	      'In process'
-1         	2	     3	               2	        2	                 2	               Array(2)    '$678.00'	   'July 7, 2021'	      'Billed'
-2	          3	     1	               2	        4	                 4	               Array(1)	   '$85.00'	     'Dec 10, 2022'	      'In process'
-
-Array(3)
-  > 0: {id: 1, orderStatusId: 1, userId: 2, shippingAddress: 3, billingAddress: 4, …}
-  > 1: {id: 2, orderStatusId: 3, userId: 2, shippingAddress: 2, billingAddress: 2, …}
-  > 2: {id: 3, orderStatusId: 1, userId: 2, shippingAddress: 4, billingAddress: 4, …}
-    length: 3
-  > [[Prototype]]: Array(0)
-  
-  */
